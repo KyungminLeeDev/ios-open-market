ca98debdfc514e02dfcb6496429aacdb4fdbd5a2
iluxsm <be9.smile@gmail.com>
2021-01-29 03:36:49 +0900

    Feat #13 - Segmented Controller에서 사용할 TableViewController 구현 및 Xib 연결

--- OpenMarket/OpenMarket/New Group/ListViewController.swift
+++ OpenMarket/OpenMarket/New Group/ListViewController.swift
@@ -9,21 +9,25 @@ import UIKit
 
 class ListViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    let nib = UINib(nibName: "ListViewCell", bundle: nil)
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.dataSource = self
+        tableView.register(nib, forCellReuseIdentifier: "itemCell")
+    }
+}
 
-        // Do any additional setup after loading the view.
+extension ListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
     }
 
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "itemCell") else {
+            return UITableViewCell()
+        }
+        return cell
     }
-    */
-
 }
