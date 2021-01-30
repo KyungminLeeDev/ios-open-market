876c4206baf1cab8318b32bb9ae06456ad82f9c4
iluxsm <be9.smile@gmail.com>
2021-01-31 05:59:42 +0900

    Chore #13 - 커스텀 Grid 타입을 만들기 위한 View 파일 및 xib 생성

---
+++ OpenMarket/OpenMarket/CustomCell/GridViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  GridViewCell.swift
+//  OpenMarket
+//
+//  Created by iluxsm on 2021/01/30.
+//
+
+import UIKit
+
+class GridViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
