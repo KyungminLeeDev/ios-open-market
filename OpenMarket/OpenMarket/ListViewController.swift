daba9dd9ed4aa334f795254310b14f0a2baa4b77
iluxsm <be9.smile@gmail.com>
2021-02-09 01:41:19 +0900

    Feat #13 - ListViewController 변경사항
    [Description]
    * ContainerVC에서 오는 Notification을 수신할 수 있는 Observer 추가
    * 요구 사항에 맞게 리스트 되도록 기능 구현

--- OpenMarket/OpenMarket/ListViewController.swift
+++ OpenMarket/OpenMarket/ListViewController.swift
@@ -11,13 +11,7 @@ class ListViewController: UIViewController {
 
     // MARK: - IBOutlets & Properties
     @IBOutlet weak var tableView: UITableView!
-    var referenceCount: Int = 0
-
-    // MARK: - IBActions & Methods
-    func registerXib() {
-        let xib: UINib = UINib(nibName: "ListViewCell", bundle: nil)
-        tableView.register(xib, forCellReuseIdentifier: "itemListCell")
-    }
+    private var pageData: MarketPage?
 
     // MARK: - View lifecycle
     override func viewDidLoad() {
@@ -26,15 +20,36 @@ class ListViewController: UIViewController {
         tableView.dataSource = self
 
         registerXib()
+        NotificationCenter.default.addObserver(self, selector: #selector(didReceiveDataNotification(_:)), name: .DidReceiveDataNotification, object: nil)
     }
 
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        // Data load...
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
     }
 }
 
-// MARK: - Table view extensions
+// MARK: - Extensions
+// MARK: Methods
+extension ListViewController {
+    private func registerXib() {
+        let xib: UINib = UINib(nibName: "ListViewCell", bundle: nil)
+        tableView.register(xib, forCellReuseIdentifier: "itemListCell")
+    }
+
+    @objc func didReceiveDataNotification(_ noti: Notification) {
+        guard let pageData: MarketPage = noti.userInfo?["pageData"] as? MarketPage else { return }
+        self.pageData = pageData
+        DispatchQueue.main.async {
+            self.tableView.reloadData()
+        }
+    }
+
+    private func attributedString() {
+
+    }
+}
+
+// MARK: Table view extensions
 extension ListViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         // 70 보다 값이 낮으면 답답해 보일 수 있습니다.
@@ -45,30 +60,40 @@ extension ListViewController: UITableViewDelegate {
 extension ListViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // 데이터를 받아오기 전 임시적인 값입니다.
-        return 20
+        guard let listCount: Int = pageData?.marketItems.count else { return 0 }
+        return listCount
     }
 
+    //TODO: Decrease depth & separate roles
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "itemListCell") else {
             return UITableViewCell()
         }
         guard let listCell: ListViewCell = cell as? ListViewCell else { return cell }
+        guard let items = self.pageData?.marketItems else { return listCell }
 
-        OpenMarketAPIClient().getMarketPage(pageNumber: 1) { result in
-            switch result {
-            case .success(let marketPage):
-                if self.referenceCount != marketPage.marketItems.count - 1 {
-                    DispatchQueue.main.async {
-                        listCell.itemName.text = marketPage.marketItems[self.referenceCount].title
-                        listCell.itemPrice.text = marketPage.marketItems[self.referenceCount].priceWithCurrency
-//                        listCell.itemStock.text = String(marketPage.marketItems[self.referenceCount].stock!)
-//                        listCell.discountedItemPrice.text = String(marketPage.marketItems[self.referenceCount].discountedPrice!)
-                        self.referenceCount += 1
-                    }
-                }
-            case .failure(let error):
-                print(error.localizedDescription)
-            }
+        listCell.itemName.text = items[indexPath.row].title
+
+        if items[indexPath.row].discountedPrice == nil { // 할인 적용이 안되는 상품이라면,
+            listCell.itemPrice.text = items[indexPath.row].priceWithCurrency
+            listCell.discountedItemPrice.isHidden = true
+        } else { // 할인 적용 상품이라면
+            let attributes: [NSAttributedString.Key: Any] = [.strikethroughStyle: 1, .foregroundColor: UIColor.red]
+            let attributedString: NSAttributedString = NSAttributedString(string: listCell.itemPrice.text!, attributes: attributes)
+            listCell.itemPrice.attributedText = attributedString
+            listCell.itemPrice.text = items[indexPath.row].priceWithCurrency // AttributedString 적용
+            listCell.discountedItemPrice.isHidden = false
+        }
+        listCell.discountedItemPrice.text = items[indexPath.row].discountedPriceWithCurrency
+
+        guard let itemStock: Int = items[indexPath.row].stock else { return listCell }
+        let attributesForStock: [NSAttributedString.Key: Any] = [.foregroundColor: UIColor.systemYellow]
+        let attributedStockString: NSAttributedString = NSAttributedString(string: listCell.itemStock.text!, attributes: attributesForStock)
+        if itemStock == 0 {
+            listCell.itemStock.attributedText = attributedStockString
+            listCell.itemStock.text = "품절"
+        } else {
+            listCell.itemStock.text = String(itemStock)
         }
 
         return listCell
