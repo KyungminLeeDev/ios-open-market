7b8c4f1b9dc234c0b54ff720ab43cae8fef7fd0f
iluxsm <be9.smile@gmail.com>
2021-02-09 01:32:14 +0900

    Feat #13 - ContainerVC 데이터를 NotificationCenter로 전송하도록 구현
    * 약간의 코드 리팩토링 추가

--- OpenMarket/OpenMarket/ContainerViewController.swift
+++ OpenMarket/OpenMarket/ContainerViewController.swift
@@ -12,11 +12,10 @@ class ContainerViewController: UIViewController {
     @IBOutlet weak var viewContainer: UIView!
     @IBOutlet weak var listView: UIView!
     @IBOutlet weak var gridView: UIView!
-    private let client: OpenMarketAPIClient = OpenMarketAPIClient()
-    var receivedData: MarketPage?
+    let client: OpenMarketAPIClient = OpenMarketAPIClient()
+    var indexNumber: Int = 1
 
     // MARK: - IBActions
-
     @IBAction func switchViewType(_ sender: UISegmentedControl) {
         switch sender.selectedSegmentIndex {
         case 0: // listView
@@ -32,21 +31,24 @@ class ContainerViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.viewContainer.bringSubviewToFront(listView)
-    }
-
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        self.requestItemsList()
+        self.requestItemsList(of: indexNumber)
     }
 }
 
+// MARK: - Extensions
+// MARK: Types
+extension Notification.Name {
+    static let DidReceiveDataNotification: Notification.Name = Notification.Name(rawValue: "DidReceiveData")
+}
+
+// MARK: Methods
 extension ContainerViewController {
     // 페이지 데이터 불러올건데, 별 다른 페이지 번호 요청 없으면 1페이지 보여줄거야.
-    func requestItemsList(of page: Int = 1) {
+    func requestItemsList(of page: Int) {
         self.client.getMarketPage(pageNumber: page) { result in
             switch result {
             case .success(let marketPage):
-                self.receivedData = marketPage
+                NotificationCenter.default.post(name: .DidReceiveDataNotification, object: nil, userInfo: ["pageData": marketPage])
             case .failure(let error):
                 print(error.localizedDescription)
             }
